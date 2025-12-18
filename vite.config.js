5871e58716d1fd8b0145d8c212cda5746c9bfea7
Laura Delissen <lauradelissen@hotmail.com>
2025-12-18 11:17:09 +0100

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// Zet hier de NAAM van jouw GitHub repository!
+const basePath = '/Lauratje.github.io/';
+
 export default defineConfig({
   plugins: [tailwindcss(), react() ],
-  base: '/',
+  base: basePath,
   build: {
     rollupOptions: {
       input: {
