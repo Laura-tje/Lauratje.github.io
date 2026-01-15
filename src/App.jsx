53520b9b4ec505663f612b3affb08eaa9ddc600f
Laura Delissen <lauradelissen@hotmail.com>
2026-01-15 15:35:45 +0100

    Fix images hopefully

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="min-h-screen flex flex-col bg-(--bg) text-(--text)">
       <Header />
-      <header >
+      <header className="bg-(--surface)">
       </header>
 
       <Outlet />
